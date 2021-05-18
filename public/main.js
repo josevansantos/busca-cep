9eff6694322581067691d5ffd430807cdaa48780
Josevan Santos <josevansnt@gmail.com>
2021-05-17 23:35:58 -0300

    refatorando e arrumando a DOM

--- public/main.js
+++ public/main.js
@@ -2,10 +2,11 @@ const buscarCep = async () => {
     const cep = document.getElementById('cep').value;
     const url = `http://viacep.com.br/ws/${cep}/json/`
     const endereco = await fetch(url).then((response) => response.json())
-    
+     
     document.getElementById("logradouro").value = endereco.logradouro
     document.getElementById("bairro").value = endereco.bairro
     document.getElementById("localidade").value = endereco.localidade
+    document.getElementById("uf").value = endereco.uf
 }
 
 document.getElementById('cep').addEventListener('blur', buscarCep);
